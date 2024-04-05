5382d0c4712f3fdeb44c1bdc9dac3af858b3ed8f
Olivia Statman <olivia.statman@kellogg.northwestern.edu>
2024-04-05 16:28:13 +0000

    SQL intro

--- lab-3-1.sql
+++ lab-3-1.sql
@@ -14,4 +14,6 @@
 -- | 2020 | 30       |
 -- +------+----------+
 
-
+SELECT year, name, COUNT(*)
+FROM teams
+GROUP BY year;
--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,22 @@
 -- How many lifetime hits does Barry Bonds have?
 
+--SELECT * FROM players
+--WHERE first_name = "Barry" AND last_name = "Bonds";
+--id    first_name  last_name  bats  throws
+----  ----------  ---------  ----  ------
+--1678  Barry       Bonds      L     L     
+
+--SELECT SUM(stats.hits) 
+--FROM stats 
+--INNER JOIN players ON stats.player_id = players.id 
+--WHERE players.id = 1678;
+
+SELECT SUM(stats.hits) 
+FROM players 
+INNER JOIN stats ON stats.player_id = players.id 
+WHERE players.first_name = "Barry" 
+AND players.last_name = "Bonds";
+
 -- Expected result:
 -- 2935
 
