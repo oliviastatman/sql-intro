78c687fbc6d1785559a6809c9dc0489b9d3ab70a
Olivia Statman <olivia.statman@kellogg.northwestern.edu>
2024-04-12 14:03:40 +0000

    drop create add alter

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,9 @@
 -- Drop the students table
-
+DROP TABlE IF EXISTS students; 
 -- Recreate it
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  phone TEXT
+);
